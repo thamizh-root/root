e66cbfce1abb14a08054747e8d4161c4d4299be2
Thamizh <thamizh@happy.local>
2025-10-30 10:52:46 +0530

    added scrollview

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const DATA = [
@@ -27,17 +27,23 @@ export default function Index() {
   console.log("hey");
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView>
         <View className="flex flex-column justify-center items-start h-[80px] pl-[20px]">
           <Text className="text-2xl font-JakartaExtraBold">Good Morning,</Text>
           <Text>Pierre Beshukov</Text>
         </View>
 
-{/** top navigation to choose date */}
-        <View
-          style={{
-            display: "flex",
+        {/** top navigation to choose date */}
+
+        <ScrollView
+          horizontal={true} // enables horizontal scrolling
+          showsHorizontalScrollIndicator={false} // hides the scroll bar
+          contentContainerStyle={{
             flexDirection: "row",
+            // styles applied to the content *inside* the ScrollView
+            paddingRight: 20, // Add padding to the end of the list
+          }}
+          style={{
             height: 100,
             marginTop: 10,
           }}
@@ -117,21 +123,25 @@ export default function Index() {
             <Text style={{ fontSize: 25, fontWeight: 900 }}>31</Text>
             <Text>Fri</Text>
           </View>
+        </ScrollView>
+
+        <View style={{  }}>
+          {/** based on date show tasks here (SCROLLABLE) */}
+          <ScrollView
+            horizontal={true} // enables horizontal scrolling
+            showsHorizontalScrollIndicator={false} // hides the scroll bar
+            contentContainerStyle={{
+              flexDirection: "row",
+            }}
+          >
+            <View className="flex flex-column flex-wrap ml-[20px] gap-[10] mt-[20px]">
+              {/* The task items */}
+              <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
+              <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
+              <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
+            </View>
+          </ScrollView>
         </View>
-
-{/** based on date show tasks here */}
-
-        <View className="flex flex-row flex-wrap ml-[20px] gap-[10] mt-[20px]">
-
-          <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
-          <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
-          <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
-          <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
-          <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
-          <View className="h-[140px] w-[210px] bg-blue-500 rounded-lg"></View>
-
-        </View>
-
       </SafeAreaView>
     </SafeAreaProvider>
   );
